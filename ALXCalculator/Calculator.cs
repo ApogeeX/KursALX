de24c88f0918a54d12543dfcbaa5286ec22f733a
ApogeeX <emil.palczynski@gmail.com>
2022-10-30 12:46:34 +0100

    Adding calculator

--- ALXCalculator/Calculator.cs
+++ ALXCalculator/Calculator.cs
@@ -1,10 +1,38 @@
-﻿namespace ALXCalculator
+﻿using System;
+
+namespace ALXCalculator
 {
     public class Calculator
     {
         public void Run()
         {
             Console.WriteLine("Running caluculator...");
+            Console.Write("X number: ");
+            var x = Double.Parse(Console.ReadLine());
+            Console.Write("Y number: ");
+            var y = Double.Parse(Console.ReadLine());
+            Console.WriteLine($"{x} + {y} = {Add(x,y)}");
+            Console.WriteLine($"{x} * {y} = {Multiply(x,y)}");
+            Console.WriteLine($"{x} - {y} = {Substract(x,y)}");
+            Console.WriteLine($"{x} / {y} = {Divide(x,y)}");
+        }
+
+        private double Add(double x, double y)
+        {
+            return x + y;
+        }
+
+        public double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+        public double Substract(double x, double y)
+        {
+            return x - y;
+        }
+        public double Divide(double x, double y)
+        {
+            return x / y;
         }
     }
 }
