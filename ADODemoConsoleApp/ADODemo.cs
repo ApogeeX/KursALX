30942885a7b93367771ff4a03d16802d4b026269
ApogeeX <emil.palczynski@gmail.com>
2023-01-22 14:51:56 +0100

    ADO

---
+++ ADODemoConsoleApp/ADODemo.cs
@@ -0,0 +1,60 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ADODemoConsoleApp
+{
+    public class ADODemo
+    {
+        private string _connectionString;
+
+        public ADODemo(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public void RunSelectAllFromEmployees()
+        {
+            var queryString = "select * from employees";
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var sqlCommand = new SqlCommand(queryString, connection);
+                try 
+                {
+                    connection.Open();
+                    var sqlDataReader = sqlCommand.ExecuteReader();
+
+                    while (sqlDataReader.Read())
+                    {
+                        Console.WriteLine($"{sqlDataReader[0]}, {sqlDataReader[1]}, {sqlDataReader[2]}, {sqlDataReader[3]}, {sqlDataReader[4]}");
+                    }
+                }
+                catch(Exception ex) 
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine(ex.StackTrace);
+                }
+            }
+        }
+
+        public void RunSelectDefinedColumnSet(string column1, string column2)
+        {
+            var queryString = $"select {column1}, {column2}, email from employees";
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                var sqlCommand = new SqlCommand(queryString, connection);
+
+                    connection.Open();
+                    var sqlDataReader = sqlCommand.ExecuteReader();
+
+                    while (sqlDataReader.Read())
+                    {
+                        Console.WriteLine($"{sqlDataReader[0]}, {sqlDataReader[1]}");
+                    }
+              
+            }
+        }
+    }
+}
---
+++ ADODemoConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ADODemoConsoleApp;
+
+var connectionSting = "Data Source=EMIL\\SQLEXPRESS;Initial Catalog=company;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+ADODemo demo = new ADODemo(connectionSting);
+
+//demo.RunSelectAllFromEmployees();
+//Console.WriteLine("------------------------------");
+demo.RunSelectDefinedColumnSet("first_name", "last_name");
+Console.WriteLine("------------------------------");
